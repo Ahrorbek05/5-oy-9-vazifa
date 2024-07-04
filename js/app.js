7f5531b61050b252866afe9f80a7a63f05fa9d06
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-07-04 20:50:34 +0500

    home work

--- js/app.js
+++ js/app.js
@@ -1,40 +1,59 @@
 const wrapper = document.getElementById('wrapper');
 const img = document.querySelector('.card img');
-const loader = document.querySelector('.spinner')
+const loader = document.querySelector('.spinner');
+const close = document.querySelector('.close-popup');
 
 function createCard(user) {
-    return`
+  return `
     <div class="card">
-    <img src="${user.flag}" alt="flag"/>
-    <h1>${user.country}</h1>
-    <h3>${user.id}</h3>
-    <h2>${user.code}</h2>
- </div>
-    `;
+      <img src="${user.flag}" alt="flag" class="card-image" />
+      <h1>${user.country}</h1>
+      <h3>${user.id}</h3>
+      <h2>${user.code}</h2>
+      <div class="card-overlay">
+        <img src="${user.flag}" alt="flags" class="flags-image" />
+        <span class="close-popup">&times;</span>
+      </div>
+    </div>
+  `;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://cars-pagination.onrender.com/all-countries')
-    .then(function (respone) {
-        if(respone.status == 200){
-            return respone.json()
-        }
+  fetch('https://cars-pagination.onrender.com/all-countries')
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch data');
+      }
     })
+    .then(data => {
+      if (data.length > 0) {
+        wrapper.innerHTML = data.map(user => createCard(user)).join('');
+      } else {
+        wrapper.innerHTML = '<p>No data available</p>';
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      wrapper.style.display = 'block';
+    })
+    .finally(() => {
+      loader.style.display = 'none';
+    });
+});
 
-    .then(function (data) {
-        // wrapper.style.display = 'block';
-        data.length > 0 && data.forEach(user => {
-            const card  = createCard(user);
-            wrapper.innerHTML += card; 
-        })
-    })
+document.addEventListener('click', function(event) {
+  const clickedElement = event.target;
 
-    .catch(err=> {
-        console.log(err);
-        wrapper.style.display = 'block'
-    })
-    .finally(function(){
-        loader.style.display = 'none';
-    })
+  if (clickedElement.classList.contains('card-image')) {
+    const card = clickedElement.closest('.card');
+    const overlay = card.querySelector('.card-overlay');
+    overlay.style.display = 'block';
+  }
 
-})
+  if (clickedElement.classList.contains('close-popup')) {
+    const overlay = clickedElement.closest('.card-overlay');
+    overlay.style.display = 'none';
+  }
+});
